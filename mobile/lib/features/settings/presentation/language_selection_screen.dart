6ce6e0728509946b2f4d6557611408414a1eb7de
kurdhack004-png <kurdhack004@gmail.com>
2026-09-20 11:49:39 +0300

    fix: remove unused language screen import

--- mobile/lib/features/settings/presentation/language_selection_screen.dart
+++ mobile/lib/features/settings/presentation/language_selection_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 import '../../../core/theme/app_colors.dart';
 import '../providers/settings_provider.dart';
 import '../../shell/main_shell.dart';
